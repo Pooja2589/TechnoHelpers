750f962cff37ec53c6fb4190dc468f531dc68d1a
nikhil-govind <54119683+nikhil-govind@users.noreply.github.com>
2020-09-22 12:29:17 -0700

    updated
    updated

--- Python/DesktopGame/game.py
+++ Python/DesktopGame/game.py
@@ -1,4 +1,5 @@
-#Program to create a game using python and pygame
+# Program to create a game using python and pygame
+# Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 
 import math
 import random
